2714d841d90ceb8c828200079ba3984eee2a99b3
Debby95 <debbyjacksonstar@hotmail.it>
2022-06-24 16:49:10 +0200

    crea header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <div class="container">
+            <img class="logo pt-3" src="/img/logo-small.svg" alt="">
+        </div>
+    </header>
+    
+</template>
+
+<script>
+export default {
+    name: "TheHeader",
+}
+</script>
+
+<style>
+    header {
+        background-color: #2E3A46;
+        height: 80px;
+    }
+
+    .logo {
+        width: 50px;
+    }
+</style>
+
